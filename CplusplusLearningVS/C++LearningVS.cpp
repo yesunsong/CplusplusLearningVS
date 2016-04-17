aa105892beb1893c61f6b25a72219d72fe81aba7
yesunsong <1042282500@qq.com>
2016-04-17 16:53:18 +0800

    使用函数

--- CplusplusLearningVS/C++LearningVS.cpp
+++ CplusplusLearningVS/C++LearningVS.cpp
@@ -16,46 +16,156 @@ using std::string;
 using std::vector;
 using std::time;
 
-string askText(string prompt);
-int askNumber(string prompt);
-void tellsStory(string name,string noun,int number,string bodyPart,string verb);
+// 函数声明function prototype (declaration)
+void instructions();
+//使用形参和返回值
+char askYesNo1();
+char askYesNo2(string question);
 
-/**函数应用：Mad-Lib*/
+void func();
+//使用全局变量
+int glob = 10;  // global variable
+void access_global();
+void hide_global();
+void change_global();
+//使用默认参数
+int askNumber(int high, int low = 1);
+//函数重载
+int triple(int number);
+string triple(string text);
+//使用内联函数
+int radiation(int health);
+
+/**函数*/
 int _tmain(int argc, _TCHAR* argv[]){
-	cout << "";
-	cout << "Welcome to Mad Lib.\n\n";
-	cout << "Answer the following questions to help create a new stroy.\n";
-	string name = askText("Please enter a name:");
-	string noun = askText("Please enter a plural noun:");
-	int number = askNumber("Please enter a number:");
-	string bodyPart = askText("Please enter a body part:");
-	string verb = askText("Please enter a verb:");
-	tellsStory(name,noun,number,bodyPart,verb);
+	//函数调用
+	instructions();
+
+	//使用形参和返回值
+	char answer1 = askYesNo1();
+	cout << "Thanks for answering: " << answer1 << "\n\n";
+
+	char answer2 = askYesNo2("Do you wish to save your game?");
+
+	//使用作用域
+	cout << "Thanks for answering: " << answer2 << "\n";
+	int var = 5;  // local variable in main()
+	cout << "In main() var is: " << var << "\n\n";
+
+	func();
+
+	cout << "Back in main() var is: " << var << "\n\n";
+	{
+		cout << "In main() in a new scope var is: " << var << "\n\n";
+
+		cout << "Creating new var in new scope.\n";
+		int var = 10;  // variable in new scope, hides other variable named var
+		cout << "In main() in a new scope var is: " << var << "\n\n";
+	}
+
+	cout << "At end of main() var created in new scope no longer exists.\n";
+	cout << "At end of main() var is: " << var << "\n";
+	//使用全局变量
+	cout << "In main() glob is: " << glob << "\n\n";
+	access_global();
+
+	hide_global();
+	cout << "In main() glob is: " << glob << "\n\n";
+
+	change_global();
+	cout << "In main() glob is: " << glob << "\n\n";
+
+	//使用默认参数
+	int number = askNumber(5);
+	cout << "Thanks for entering: " << number << "\n\n";
+
+	number = askNumber(10, 5);
+	cout << "Thanks for entering: " << number << "\n\n";
+
+	//函数重载
+	cout << "Tripling 5: " << triple(5) << "\n\n";
+	cout << "Tripling 'gamer': " << triple("gamer");
+	//使用内联函数
+	int health = 80;
+	cout << "Your health is " << health << "\n\n";
+
+	health = radiation(health);
+	cout << "After radiation exposure your health is " << health << "\n\n";
+
+	health = radiation(health);
+	cout << "After radiation exposure your health is " << health << "\n\n";
+
+	health = radiation(health);
+	cout << "After radiation exposure your health is " << health << "\n\n";
 	//system("pause");
 	return EXIT_SUCCESS;
 }
 
-string askText(string prompt){
-	string text;
-	cout << prompt;
-	cin >> text;
-	return text;
+//函数定义function definition
+void instructions(){
+	cout << "Welcome to the most fun you've ever had with text!\n\n";
+	cout << "Here's how to play the game...\n";
 }
 
-int askNumber(string prompt){
+char askYesNo1(){
+	char response1;
+	do	{
+		cout << "Please enter 'y' or 'n': ";
+		cin >> response1;
+	} while (response1 != 'y' && response1 != 'n');
+
+	return response1;
+}
+
+char askYesNo2(string question){
+	char response2;
+	do	{
+		cout << question << " (y/n): ";
+		cin >> response2;
+	} while (response2 != 'y' && response2 != 'n');
+
+	return response2;
+}
+
+void func(){
+	int var = -5;  // local variable in func()
+	cout << "In func() var is: " << var << "\n\n";
+}
+
+void access_global(){
+	cout << "In access_global() glob is: " << glob << "\n\n";
+}
+
+void hide_global(){
+	int glob = 0;  // hide global variable glob
+	cout << "In hide_global() glob is: " << glob << "\n\n";
+}
+
+void change_global(){
+	glob = -10;  // change global variable glob
+	cout << "In change_global() glob is: " << glob << "\n\n";
+}
+
+int askNumber(int high, int low){
 	int num;
-	cout << prompt;
-	cin >> num;
+	do	{
+		cout << "Please enter a number" << " (" << low << " - " << high << "): ";
+		cin >> num;
+	} while (num > high || num < low);
+
 	return num;
 }
 
-void tellsStory(string name, string noun, int number, string bodyPart, string verb){
-	cout << "\nHere's your story:\n";
-	cout << "The famous explorer "<<name<<" had nearly given up a life-long quest to find\n";
-	cout << "The Lost City of "<<noun<<" when one day,the "<< noun<<" found the explorer.\n";
-	cout << "Surrounded by "<<number<<" "<<noun<<",a tear came to "<<name<<"'s"<<bodyPart<<".\n" ;
-	cout << "After all this time,the quest was finally over. ";
-	cout << "And then,the "<<noun<<"\n";
-	cout << "promptly devoured "<<name<<". ";
-	cout << "The moral of the story? Be careful what you "<<verb<<" for.";
+
+
+int triple(int number){
+	return (number * 3);
+}
+
+string triple(string text){
+	return (text + text + text);
+}
+
+inline int radiation(int health){
+	return (health / 2);
 }
